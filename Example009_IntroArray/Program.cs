fa433d8d032e09a6b29bc80f64817130207f1c8e
Yana <leduschenkova6@gmail.com>
2022-09-18 01:43:25 +0300

    add Example 009 IntroArray

---
+++ Example009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿// Найдем max из 9 числел
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {16, 12, 743, 74, 53, 36, 79, 82, 19};
+
+int result = Max(Max(array[0], array[1], array[2]), Max(array[3], array[4], array[5]), Max(array[6], array[7], array[8]));
+
+Console.WriteLine(result);
+
+
